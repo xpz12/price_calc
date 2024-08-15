18dc03ac64cf3cfe6608bb9e58fe0d955c785ea4
xpz12 <141086740+xpz12@users.noreply.github.com>
2024-08-15 17:57:20 +0300

    added new num prefixes

--- price.py
+++ price.py
@@ -5,8 +5,8 @@ def calculate_final_price(initial_price, num_pricing):
     current_price = initial_price
     
     for _ in range(num_pricing):
-        final_price += current_price  # Складываем цену
-        current_price += current_price * 0.11  # Складываем цену и уменьшаем на 11%
+        current_price += current_price * 0.11  # Уменьшаем текущую цену на 11%
+        final_price += current_price  # Складываем уменьшенную цену
 
     # Округляем конечную сумму и форматируем с разделением тысяч
     formatted_price = f"{round(final_price):,}".replace(",", ".")
@@ -17,8 +17,7 @@ def calculate_final_price(initial_price, num_pricing):
     return f"{formatted_price} ({formatted_with_units})"   
 
 def format_with_units(value):
-    units = [("Qi", 1e18), ("Qa", 1e15), ("T", 1e12), ("B", 1e9), ("M", 1e6)]
-    
+    units = [("Oc", 1e27), ("Sp", 1e24), ("Sx", 1e21),("Qi", 1e18), ("Qa", 1e15), ("T", 1e12), ("B", 1e9), ("M", 1e6)]
     for unit, threshold in units:
         if value >= threshold:
             return f"{value / threshold:.2f} {unit}"
